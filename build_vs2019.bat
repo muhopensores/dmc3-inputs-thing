8d828a07be6eb3d77cf71dc002b143e187247ffb
SSSiyan <gsssiyan@gmail.com>
2021-10-18 17:43:56 +0100

    Update build_vs2019.bat

--- build_vs2019.bat
+++ build_vs2019.bat
@@ -4,7 +4,7 @@ REM CLANG LINE
 REM cmake .. -G "Visual Studio 16 2019" -T ClangCl -DDEVELOPER_MODE=OFF
 REM ICL LINE
 REM cmake .. -G "Visual Studio 16 2019" -T "Intel C++ Compiler 19.0" -DDEVELOPER_MODE=OFF
-cmake .. -G "Visual Studio 16 2019" -T ClangCl -DDEVELOPER_MODE=OFF
+cmake .. -G "Visual Studio 16 2019" -A "Win32" -DDEVELOPER_MODE=OFF
 cmake --build . --config Release
 cd ..
 pause
